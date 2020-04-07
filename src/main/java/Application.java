0e14f108870b7ac0e8c64476b3cd9a15a0a2fad5
shasha <shashasi8023@163.com>
2020-04-07 09:08:26 +0800

    [shasha] refactor: fetch method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -68,28 +68,7 @@ public class Application {
   }
 
   public static String fetch(String ticket) {
-    //TODO 判断ticket的编号
-    String sql = "SELECT car_number carNumber FROM ticket_A WHERE spot_id = ?";
-    List<String> ticketInfo = Arrays.asList(ticket.split(","));
-    int spotId = Integer.parseInt(ticketInfo.get(1));
-    List<Car> list = PreparedStatementQuery.queryInfoList(Car.class, sql, spotId);
-    assert list != null;
-    if (list.size() > 0) {
-      sql = "DELETE FROM ticket_A WHERE spot_id = ?";
-      PreparedStatementUpdate.update(sql, spotId);
-      System.out.println("已为您取到车牌号为" + list.get(0).getCarNumber() + "的车辆，很高兴为您服务，祝您生活愉快!");
-      return list.get(0).getCarNumber();
-    } else {
-      sql = "SELECT car_number carNumber FROM ticket_B WHERE spot_id = ?";
-      list = PreparedStatementQuery.queryInfoList(Car.class, sql, spotId);
-    }
-    assert list != null;
-    if (list.size() > 0) {
-      sql = "DELETE FROM ticket_B WHERE spot_id = ?";
-      PreparedStatementUpdate.update(sql, spotId);
-      System.out.println("已为您取到车牌号为" + list.get(0).getCarNumber() + "的车辆，很高兴为您服务，祝您生活愉快!");
-      return list.get(0).getCarNumber();
-    }
-    throw new InvalidTicketException("很抱歉，无法通过您提供的停车券为您找到相应的车辆，请您再次核对停车券是否有效！");
+    Manager manager = new Manager();
+    return manager.manageFetch(ticket);
   }
 }
